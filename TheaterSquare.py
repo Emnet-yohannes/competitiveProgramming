dbc61ac3612616b723eb1ffae804104f55ec7162
Emnet-yohannes <emnet.yohan@gmail.com>
2022-01-07 23:52:44 +0300

    theater square

--- TheaterSquare.py
+++ TheaterSquare.py
@@ -1,8 +1,17 @@
 import math
 def TheaterSquare(n,m,a):
     #print(math.floor((n*m)/2))
-    return 2*math.floor((n*m)/pow(2,a))
+    if(n%a==0):
+        d1=n//a
+    else:
+        d1 = n//a +1
+    if(m%a==0):
+        d2 = m // a
+    else :
+        d2 = m //a +1
+    print(d1*d2)
+    
 
 n,m,a= list(map(int, input().split()))
 # print(TheaterSquare(n,m,a))
-TheaterSquare(6,6,4)
+# TheaterSquare(6,6,4)
